76783a5e62fb2b6be9d298b318619515cc4ed0d9
David Cussans <david.cussans@cern.ch>
2022-03-04 15:01:16 +0000

    Replaced si53x4 --> si5395 and D-FF

--- hardware/Cadence/temp/packagerxl.sh
+++ hardware/Cadence/temp/packagerxl.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 CDS_CONCEPT_HDL=TRUE;export CDS_CONCEPT_HDL
-cd /projects/HEP_Instrumentation/cad/designs/uob-hep-pc069/hardware/Cadence
+cd /data/CAD/Cadence/designs/uob-hep-pc069/hardware/Cadence
 netassembler
-concept2cm -forward -export -proj "/projects/HEP_Instrumentation/cad/designs/uob-hep-pc069/hardware/Cadence/pc069a_toplevel.cpm"
-pxl.exe -proj "/projects/HEP_Instrumentation/cad/designs/uob-hep-pc069/hardware/Cadence/pc069a_toplevel.cpm" -nosavehier
+concept2cm -forward -export -proj "/data/CAD/Cadence/designs/uob-hep-pc069/hardware/Cadence/pc069a_toplevel.cpm"
+pxl.exe -proj "/data/CAD/Cadence/designs/uob-hep-pc069/hardware/Cadence/pc069a_toplevel.cpm" -nosavehier
